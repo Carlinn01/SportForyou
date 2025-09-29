eb95a5ed86b085c62e8108a70eb68469c426ec12
Carlinn01 <carloseduardodalazenoliveira@gmail.com>
2025-09-29 09:28:33 -0300

    Login e cadastro funcionando

--- login/src/UsuarioDAO.php
+++ login/src/UsuarioDAO.php
@@ -1,25 +1,38 @@
 <?php
-require "ConexaoBD.php";
+require_once "ConexaoBD.php";
+require_once "Util.php";
 
-class UsuarioDAO{
+class UsuarioDAO {
 
-    public static function cadastrarUsuario($dados){
+    public static function cadastrarUsuario($dados) {
         $conexao = ConexaoBD::conectar();
+
+        $email = $dados['email'];
+        $senha = $dados['senha'];
+        $nome = $dados['nome'];
+        $nome_usuario = $dados['nome_usuario'];
+        $nascimento = $dados['nascimento'];
         
-        $sql = "insert into usuarios (email, senha) values (?,?)";
+        $foto_perfil = Util::salvarArquivo();
+
+        $sql = "INSERT INTO usuarios (email, senha, nome, nome_usuario, nascimento, foto_perfil) VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = $conexao->prepare($sql);
-        
-        $stmt->bindParam(1, $dados['email']);
-        $senhaCriptografada = md5($dados['senha']);
+
+        $senhaCriptografada = password_hash($senha, PASSWORD_BCRYPT);
+
+        $stmt->bindParam(1, $email);
         $stmt->bindParam(2, $senhaCriptografada);
+        $stmt->bindParam(3, $nome);
+        $stmt->bindParam(4, $nome_usuario);
+        $stmt->bindParam(5, $nascimento);
+        $stmt->bindParam(6, $foto_perfil);
 
         $stmt->execute();
     }
 
-    public static function validarUsuario($dados){
-        
+    public static function validarUsuario($dados) {
         $senhaCriptografada = md5($dados['senha']);
-        $sql = "select * from usuarios where email=? AND senha=?";
+        $sql = "SELECT * FROM usuarios WHERE email=? AND senha=?";
 
         $conexao = ConexaoBD::conectar();
         $stmt = $conexao->prepare($sql);
