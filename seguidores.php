90a5ee34702cfa275611914d4b3c33c12c3e3db2
Carlinn01 <carloseduardodalazenoliveira@gmail.com>
2025-10-06 08:36:18 -0300

    Adicionado pagina seguidores

---
+++ seguidores.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicione seguidores</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <h1>Adicione Seguidores!</h1>
+
+        <h3>Nome de Usuário</h3>
+        <label for="">Nome de Usuário</label>
+        <input type="text" placeholder="Nome de Usuário"> 
+        <button>Buscar</button>
+
+        
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
